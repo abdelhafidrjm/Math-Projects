7f71d6ef429781340eda5316fb014370e07944fb
Abdelhafid <abdelhafid.redjem07@gmail.com>
2026-08-12 17:25:28 +0100

    deleted unwanted files

--- Calculator/essay func.py
+++
@@ -1,9 +0,0 @@
-def coundDigits(n):
-    count = 0
-    while n != 0:
-        n = (n - n % 10) / 10
-        count += 1
-    return count
-
-num = int(input())
-print(coundDigits(num))
--- Calculator/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-# buttonpls.grid(row = 1, column = 3,  padx = 1, pady = 1, sticky = "e")
-# buttonmin.grid(row = 1, column = 4,  padx = 1, pady = 1, sticky = "w")
-# buttonmul.grid(row = 2, column = 3,  padx = 1, pady = 1, sticky = "e")
-# buttondiv.grid(row = 2, column = 4,  padx = 1, pady = 1, sticky = "w")
-# buttoneql.grid(row =
